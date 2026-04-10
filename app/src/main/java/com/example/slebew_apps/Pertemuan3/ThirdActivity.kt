08fc5a2705c6b68a50346b15bea0c573d6a3829c
Johan24si <johan24si@mahasiswa.pcr.ac.id>
2026-04-10 10:40:13 +0700

    p3

---
+++ app/src/main/java/com/example/slebew_apps/Pertemuan3/ThirdActivity.kt
@@ -0,0 +1,41 @@
+package com.example.slebew_apps.Pertemuan3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.slebew_apps.R
+import com.example.slebew_apps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.buttonSubmit.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nama = binding.inputNama.text
+
+
+            Toast.makeText(this, "Anda telah melakukan klik pada tombol Submit", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
