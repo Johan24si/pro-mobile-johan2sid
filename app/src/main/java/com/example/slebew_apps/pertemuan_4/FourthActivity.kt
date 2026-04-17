8c5a79ed7152ba055e77195e273d3e8401b7c434
Johan24si <johan24si@mahasiswa.pcr.ac.id>
2026-04-17 07:59:17 +0700

    p4 - warming up

--- app/src/main/java/com/example/slebew_apps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/slebew_apps/pertemuan_4/FourthActivity.kt
@@ -1,31 +1,24 @@
-package com.example.slebew_apps
+package com.example.slebew_apps.pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.slebew_apps.databinding.ActivityMainBinding
-import com.example.slebew_apps.MainActivity
 import com.example.slebew_apps.pertemuan_4.FourthActivity
+import com.example.slebew_apps.R
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_fourth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        binding.buttonSubmit.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            startActivity(intent)
         }
     }
 }
